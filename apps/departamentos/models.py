dcbfe1ea5b19d963b8188d5c1791c865960b41ba
Wenderson Wanzeller <wenderson@wanzeller.com>
2024-01-01 17:43:52 +0000

    CRUD Departamentos

--- apps/departamentos/models.py
+++ apps/departamentos/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
+from apps.empresas.models import Empresa
+
 
 class Departamento(models.Model):
     nome = models.CharField(max_length=100, help_text='Nome do Departamento')
+    empresa = models.ForeignKey(Empresa, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.nome
