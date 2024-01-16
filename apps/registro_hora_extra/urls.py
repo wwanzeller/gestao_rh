ba26c19aeabeb30cba8652a9c9d5047cfa045ea8
Wenderson Wanzeller <wenderson@wanzeller.com>
2024-01-16 15:13:20 +0000

    Cria CRUD horas extras

--- apps/registro_hora_extra/urls.py
+++ apps/registro_hora_extra/urls.py
@@ -1,11 +1,17 @@
 # from django.http import HttpResponse
 from django.urls import path
 from .views import (
+    HoraExtraCreate,
     HoraExtraList,
+    HoraExtraUpdate,
+    HoraExtraDelete,
 )
 
 
 urlpatterns = [
     # path('', lambda request: HttpResponse("Olá desde horas extras")),
+    path('criar/', HoraExtraCreate.as_view(), name='horaextra_create'),
+    path('atualizar/<int:pk>/', HoraExtraUpdate.as_view(), name='horaextra_update'),
     path('', HoraExtraList.as_view(), name='horaextra_list'),
+    path('deletar/<int:pk>', HoraExtraDelete.as_view(), name='horaextra_delete')
 ]
--- apps/registro_hora_extra/views.py
+++ apps/registro_hora_extra/views.py
@@ -1,7 +1,11 @@
+from django.urls import reverse_lazy
 from django.views.generic import (
     ListView,
+    UpdateView,
+    DeleteView, CreateView
 )
 from apps.registro_hora_extra.models import RegistroHoraExtra
+from apps.funcionarios.models import Funcionario
 
 
 class HoraExtraList(ListView):
@@ -10,3 +14,29 @@ class HoraExtraList(ListView):
     def get_queryset(self):
         empresa_logada = self.request.user.funcionario.empresa
         return RegistroHoraExtra.objects.filter(funcionario__empresa=empresa_logada)
+
+
+class HoraExtraUpdate(UpdateView):
+    model = RegistroHoraExtra
+    fields = ['motivo', 'horas']
+
+    def get_success_url(self):
+        return reverse_lazy('horaextra_update', args=[self.object.id])
+
+
+class HoraExtraDelete(DeleteView):
+    model = RegistroHoraExtra
+    success_url = reverse_lazy('horaextra_list')
+
+
+class HoraExtraCreate(CreateView):
+    model = RegistroHoraExtra
+    fields = ['motivo', 'horas', 'funcionario']
+    success_url = reverse_lazy('horaextra_list')
+
+    def get_form(self):
+        form = super().get_form()
+        form.fields['funcionario'].queryset = Funcionario.objects.filter(
+            empresa=self.request.user.funcionario.empresa
+        )
+        return form
